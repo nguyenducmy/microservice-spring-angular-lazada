6a5e60f8f43f354124dec1fc7f808456932153f0
myn1008199x@gmail.com <myn1008199x@gmail.com>
2023-05-27 02:49:05 +0700

    create lazada home page

---
+++ LazadaUI/src/models/authen-response.ts
@@ -0,0 +1,17 @@
+export class AuthenResponse {
+  private code: string;
+  private object: any;
+  private status: string | undefined
+
+  constructor(private res: any) {
+    this.code = res.code;
+    this.object = res.object.toString();
+    this.status = res.status;
+  }
+  get getJwtToken():string{
+    return <string>this.object;
+  }
+  get getCode():string{
+    return <string>this.code;
+  }
+}
