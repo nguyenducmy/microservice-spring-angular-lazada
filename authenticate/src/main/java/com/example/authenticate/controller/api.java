9afec57e2e5a87c0b2033c27b27c324d82f2ec4e
ducmy <mynguyenduc9x@gmail.com>
2023-06-09 07:53:04 +0700

    fix null pointer exception validate user

--- authenticate/src/main/java/com/example/authenticate/controller/api.java
+++ authenticate/src/main/java/com/example/authenticate/controller/api.java
@@ -10,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/v1/authen")
 @CrossOrigin(origins = "http://localhost:4200/")
@@ -21,7 +23,7 @@ public class api {
 
     @PostMapping("/register")
     public ResponseEntity<BaseResponse> register(@RequestBody User user) {
-        User isUser = userRepository.findByUsername(user.getUsername()).orElseThrow();
+        Optional<User> isUser = userRepository.findByUsername(user.getUsername());
         BaseResponse baseResponse = new BaseResponse();
         if(isUser == null){
             String password = new BCryptPasswordEncoder().encode(user.getPassword());
