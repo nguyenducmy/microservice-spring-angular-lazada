aa99211483df19c2f6ab8065ab1c5ae68592d962
mynguyenduc9x <mynguyenduc9x@gmail.com>
2023-06-12 03:45:43 +0700

    add vendors to LazadaUI

---
+++ paper-validate/src/test/java/com/example/papervalidate/PaperValidateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.papervalidate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperValidateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
